21b67b00766d562e08f8324f0d1b6cd1833ebb1e
DRucci-88 <60127414+DRucci-88@users.noreply.github.com>
2023-02-15 21:44:16 +0700

    Providers, Provider, StateProvider, StateNotifier, and ChangeNOtifier

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,23 +1,117 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_learning/main.dart';
+import 'package:riverpod_learning/user.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
+  Widget sb() => const SizedBox(height: 10);
+
+  void onSubmitName(WidgetRef ref, String value) {
+    ref.read(nameStateProvider.notifier).update((state) => value);
+  }
+
+  void onSubmitUserName(WidgetRef ref, String value) {
+    ref.read(userNotifierProvider.notifier).updateName(value);
+  }
+
+  void onSubmitUserAge(WidgetRef ref, int value) {
+    ref.read(userNotifierProvider.notifier).updateAge(value);
+  }
+
+  void onSubmitUserNameNotifier(WidgetRef ref, String value) {
+    ref.read(userChangeNotifierProvider).updateName(value);
+  }
+
+  void onSubmitUserAgeNotifier(WidgetRef ref, int value) {
+    ref.read(userChangeNotifierProvider).updateAge(value);
+  }
+
   @override
-  Widget build(BuildContext context, WidgetRef ref) { 
-    final name = ref.watch(nameProvider);
-    final nameRead = ref.read(nameProvider); // one time only
+  Widget build(BuildContext context, WidgetRef ref) {
+    ///
+    final String name = ref.watch(nameProvider);
+
+    /// Read-Only
+    /// one time only and better inside initState
+    final String nameRead = ref.read(nameProvider);
+
+    /// Simple Provider
+    final String nameState = ref.watch(nameStateProvider) ?? "Null";
+
+    /// Complex provider
+    final User user = ref.watch(userNotifierProvider);
+
+    /// Complex Provider
+    /// But only selected one property that change and can trigger rebuild UI
+    final String userName = ref.watch(userNotifierProvider.select(
+      (value) => value.name,
+    ));
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(name),
+        title: Text(nameRead),
       ),
       body: Center(
         child: Column(
           children: [
             Text(name),
-            Text(nameRead),
+            sb(),
+            TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                label: Text('Name'),
+              ),
+              onSubmitted: (value) => onSubmitName(ref, value),
+            ),
+            sb(),
+            Text(nameState),
+            sb(),
+
+            /// State Notifier
+            TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                label: Text('User Name'),
+              ),
+              onSubmitted: (value) => onSubmitUserName(ref, value),
+            ),
+            sb(),
+            Text(user.name.toString()),
+            sb(),
+            TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                label: Text('User Age'),
+              ),
+              onSubmitted: (value) => onSubmitUserAge(ref, int.parse(value)),
+            ),
+            sb(),
+            Text(user.age.toString()),
+
+            /// Change Notifier
+            sb(),
+            TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                label: Text('User Name Change Notifier'),
+              ),
+              onSubmitted: (value) => onSubmitUserNameNotifier(ref, value),
+            ),
+            sb(),
+            Text(user.name.toString()),
+            sb(),
+            TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                label: Text('User Age Change Notifier'),
+              ),
+              onSubmitted: (value) =>
+                  onSubmitUserAgeNotifier(ref, int.parse(value)),
+            ),
+            sb(),
+            Text(user.age.toString()),
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_learning/home_screen.dart';
 import 'package:riverpod_learning/home_screen_1.dart';
+import 'package:riverpod_learning/user.dart';
 
+/// Provider
+/// Providers
+/// StateProvider
+/// StateNotifier and StateNotifierProvider
+/// ChangeNotifier and ChangeNotifierProvider
+///   -> Only transition from Provider to Riverpod
+///   -> Backward compatible for user that use Provider state management
+///   -> Not recomended by Riverpod docs to use this anymore
+///   -> Do not have access to data model and state
+///   -> Use notifyListeners(); inside method to update UI.
+///   -> This provider is MUTABLE (very bad), it can change its property outside the class
+
+/// Read-Only Provider
+/// Maybe good for constant value across entire apps
 final nameProvider = Provider<String>((ref) {
   return "rucci";
+});
+
+/// Simple Provider
+/// Good for update a simple value
+/// Rare to use cause apps usualy have a complex data and need its provider
+final nameStateProvider = StateProvider<String?>((ref) {
+  return null;
+});
+
+/// Best Approuch for complex
+final userNotifierProvider = StateNotifierProvider<UserNotifier, User>((ref) {
+  // return UserNotifier(const User(name: '', age: 0));
+  return UserNotifier();
+});
+
+final userChangeNotifierProvider = ChangeNotifierProvider((ref) {
+  return UserNotifierProvider();
 });
 
 void main() {
